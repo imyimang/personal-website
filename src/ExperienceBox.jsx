fc4ee2237029685ecd3e0970aad124d1790ea2f1
yimang <lucasbbtung@gmail.com>
2024-06-03 21:37:24 +0800

    整理排版

--- src/ExperienceBox.jsx
+++ src/ExperienceBox.jsx
@@ -3,13 +3,16 @@ import React from "react";
 const ExperienceBox = () => {
   return (
     <div className="bg-white rounded-lg shadow-2xl p-8 w-full xl:max-w-10xl h-40 mx-auto flex flex-col justify-center items-center experience-box transition-transform duration-300 hover:scale-110">
-    <h2 className="text-black text-2xl font-bold text-center font-sans">
-      經歷
-    </h2>
-    <p className="text-gray-700 text-center font-sans">
-      <a href="/coffeehost" className="text-blue-500 hover:underline">CoffeeHost超大咖託管</a> 團隊成員 2024/2月~Now
-    </p>
-  </div>
+      <h2 className="text-black text-2xl font-bold text-center font-sans">
+        經歷
+      </h2>
+      <p className="text-gray-700 text-center font-sans">
+        <a href="/coffeehost" className="text-blue-500 hover:underline">
+          CoffeeHost超大咖託管
+        </a>{" "}
+        團隊成員 2024/2月~Now
+      </p>
+    </div>
   );
 };
 
