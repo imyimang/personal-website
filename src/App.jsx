351e1871398a59891026235be7be30621becada0
yimang <lucasbbtung@gmail.com>
2024-06-04 21:07:17 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./home//Homepage";
-import Coffeehost from "./Coffeehost/Coffeehost";
+import Coffeehost from "./Coffeehost/Coffeehostpage";
 
 
 function App() {
