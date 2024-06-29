e3eb0e93d10870b1636b3ced3f2552049771b877
yimang <lucasbbtung@gmail.com>
2024-06-29 22:21:38 +0800

    delete blog page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./home//Homepage";
 import Coffeehost from "./Coffeehost/Coffeehostpage";
-import Blog from "./blog/blog_list";
-import Dashboard from "./blog/blog_write";
-
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage/>} />
-        <Route path="/blog" element={<Blog />} />
         <Route path="/coffeehost" element={<Coffeehost />} />
-        <Route path="/write" element={<Dashboard />} />
       </Routes>
     </Router>
   );
