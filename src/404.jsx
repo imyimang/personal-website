4f9cd0f8263df6312066a3145f22f49467a265d2
yimang <lucasbbtung@gmail.com>
2024-07-06 10:47:53 +0800

    Update 404.jsx

--- src/404.jsx
+++ src/404.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Title from './Title';
+import Mouse from './Mouse'
 
 const NotFound = () => {
   return (
 
       <div className="text-center">
         <Title title = "404" top = "40"/>
+        <Mouse/>
         <p className="text-2xl text-gray-600 mb-8">頁面未找到</p>
         <Link to="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
           返回首頁
