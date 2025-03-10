91bb47e709a9e27fc22ba501ce558c18eca75f26
yimang <lucasbbtung@gmail.com>
2025-03-10 23:17:45 +0800

    Update SocialBox.jsx

--- src/home/SocialBox.jsx
+++ src/home/SocialBox.jsx
@@ -17,7 +17,7 @@ const SocialBox = () => {
   };
 
   return (
-    <div className="bg-white rounded-lg shadow-2xl p-5 w-full max-w-5/6 h-40 mx-auto flex justify-around items-center px-10 transition-transform duration-400 hover:scale-110">
+    <div className="bg-white rounded-lg shadow-2xl p-5 w-full max-w-[83.333%] h-40 mx-auto flex justify-around items-center px-10 transition-transform duration-400 hover:scale-110">
       <a
         href="https://www.instagram.com/yimang__/"
         target="_blank"
